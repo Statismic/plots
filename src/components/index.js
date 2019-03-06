90e69d12d19c5964a31f119cf21723d45cdf6ea3
Lukas Herman <lherman.cs@gmail.com>
2019-03-06 18:48:32 -0500

    Exposed plot component

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,9 @@
 import Plot from "./Plot.vue";
 
-const components = {
-  Plot
-};
+export { Plot };
 
 export default {
   install(Vue) {
-    Object.keys(components).forEach(name => {
-      Vue.component(name, components[name]);
-    });
+    Vue.component("plot", Plot);
   }
 };
